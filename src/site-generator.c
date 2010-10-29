3691ab7e3bdd8176c8806e4c13b4566c2bad86aa
erenon <erenon2@gmail.com>
2010-10-29 23:32:36 +0200

    main added

---
+++ src/site-generator.c
@@ -0,0 +1,20 @@
+/*
+ * site-generator.c
+ *
+ *  Created on: 2010.10.24.
+ *      Author: erenon
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "genlib.h"
+#include "dir.h"
+
+int main(int argc, char *argv[]) {
+	//read index
+	Dir *dir;
+
+	dir = dir_create("");
+
+	return EXIT_SUCCESS;
+}
